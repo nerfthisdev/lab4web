863726d8f41c1330a7063fa58a0fd69de3235767
nerfthisdev <353054@niuitmo.ru>
2024-12-12 21:22:46 +0300

    ✨ feat: rewrite radios in apppage to options variant

--- frontend/lab-app/src/pages/AppPage.tsx
+++ frontend/lab-app/src/pages/AppPage.tsx
@@ -7,14 +7,31 @@ export function AppPage() {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="box-with-inputs bg-dark text-white p-4 rounded shadow-sm">
-        <TextInputWithLabel inputId="y1">Y:</TextInputWithLabel>
-        <TextInputWithLabel inputId="y2">Y:</TextInputWithLabel>
+        <TextInputWithLabel inputId="y1" label="Y:" />
+        <TextInputWithLabel inputId="y2" label="Y:" />
         <RadioFieldSet
-          items={["1", "2", "3", "4", "5"]}
-          itemsvalues={["1", "2", "3", "4", "5"]}
-        >
-          Radios:{" "}
-        </RadioFieldSet>
+          legend="Radios"
+          options={[
+            {
+              id: "gridRadios1",
+              value: "option1",
+              label: "First radio",
+              disabled: false,
+            },
+            {
+              id: "gridRadios2",
+              value: "option2",
+              label: "Second radio",
+              disabled: false,
+            },
+            {
+              id: "gridRadios3",
+              value: "option3",
+              label: "Third disabled radio",
+              disabled: true,
+            },
+          ]}
+        />
       </div>
       <div className="ms-4 rounded-4 border border-1 overflow-hidden">
         <Graph />
