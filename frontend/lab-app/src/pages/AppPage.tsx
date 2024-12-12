24923bc7d9bc4f617df1a6658df797957dd720ce
nerfthisdev <353054@niuitmo.ru>
2024-12-12 21:57:00 +0300

    ✨ feat: add form and FormSubmitButton

--- frontend/lab-app/src/pages/AppPage.tsx
+++ frontend/lab-app/src/pages/AppPage.tsx
@@ -1,3 +1,4 @@
+import { FormSubmitButton } from "../components/FormSubmitButton";
 import { Graph } from "../components/Graph";
 
 import { RadioFieldSet } from "../components/RadioFieldSet";
@@ -7,17 +8,20 @@ export function AppPage() {
   const values = ["1", "2", "3", "4", "5"];
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="box-with-inputs bg-dark text-white p-4 rounded shadow-sm">
-        <TextInputWithLabel inputId="x2" label="X:" />
-        <TextInputWithLabel inputId="y2" label="Y:" />
-        <RadioFieldSet items={values} itemsvalues={values}>
-          R:
-        </RadioFieldSet>
+    <form>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="box-with-inputs bg-dark text-white p-4 rounded shadow-sm">
+          <TextInputWithLabel inputId="x2" label="X:" />
+          <TextInputWithLabel inputId="y2" label="Y:" />
+          <RadioFieldSet items={values} itemsvalues={values}>
+            R:
+          </RadioFieldSet>
+        </div>
+        <div className="ms-4 rounded-4 border border-1 overflow-hidden">
+          <Graph />
+        </div>
       </div>
-      <div className="ms-4 rounded-4 border border-1 overflow-hidden">
-        <Graph />
-      </div>
-    </div>
+      <FormSubmitButton>Submit</FormSubmitButton>
+    </form>
   );
 }
