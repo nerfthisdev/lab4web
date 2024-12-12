67160a2be9d923e8eda618f9baf3a2dddec60d4a
nerfthisdev <353054@niuitmo.ru>
2024-12-12 23:34:14 +0300

    🐞 fix: added missing key attribute

--- frontend/lab-app/src/components/RadioFieldSet.tsx
+++ frontend/lab-app/src/components/RadioFieldSet.tsx
@@ -12,14 +12,14 @@ export function RadioFieldSet({ items, itemsvalues, children }: Props) {
       <legend className="col-form-label col-sm-2 pt-0">{children}</legend>
       <div className="col-sm-10">
         {items.map((items, index) => (
-          <div className="form-check">
+          <div className="form-check" key={items}>
             <input
               className="form-check-input"
               type="radio"
               name="gridRadios"
               id={`gridRadios${index}`}
               value={itemsvalues[index]}
-              checked
+              defaultChecked={index === 0 ? true : false}
             />
             <label className="form-check-label" htmlFor={`gridRadios${index}`}>
               {items}
--- frontend/lab-app/src/pages/AppPage.tsx
+++ frontend/lab-app/src/pages/AppPage.tsx
@@ -8,20 +8,21 @@ export function AppPage() {
   const values = ["1", "2", "3", "4", "5"];
 
   return (
-    <form>
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="box-with-inputs bg-dark text-white p-4 rounded shadow-sm">
-          <TextInputWithLabel inputId="x2" label="X:" />
-          <TextInputWithLabel inputId="y2" label="Y:" />
-          <RadioFieldSet items={values} itemsvalues={values}>
-            R:
-          </RadioFieldSet>
-        </div>
-        <div className="ms-4 rounded-4 border border-1 overflow-hidden">
-          <Graph />
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="box-with-inputs bg-dark text-white p-4 rounded shadow-sm">
+        <TextInputWithLabel inputId="x2" label="X:" />
+        <TextInputWithLabel inputId="y2" label="Y:" />
+        <RadioFieldSet items={values} itemsvalues={values}>
+          R:
+        </RadioFieldSet>
+
+        <div className="d-flex justify-content-center">
+          <FormSubmitButton>Submit</FormSubmitButton>
         </div>
       </div>
-      <FormSubmitButton>Submit</FormSubmitButton>
-    </form>
+      <div className="ms-4 rounded-4 border border-1 overflow-hidden">
+        <Graph />
+      </div>
+    </div>
   );
 }
