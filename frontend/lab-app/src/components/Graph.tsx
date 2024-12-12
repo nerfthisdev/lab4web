c9bd162d3035e63a5692c26848aa7515d91d6403
nerfthisdev <353054@niuitmo.ru>
2024-12-12 23:33:51 +0300

    ✨ feat: added sector and triangle graphs

--- frontend/lab-app/src/components/Graph.tsx
+++ frontend/lab-app/src/components/Graph.tsx
@@ -1,9 +1,28 @@
-import { Mafs, Coordinates } from "mafs";
+import { Mafs, Coordinates, Polyline, Theme, Polygon, Plot } from "mafs";
 
 export function Graph() {
+  const a = [-2.5, 0] as [number, number];
+  const b = [0, 5] as [number, number];
+  const c = [0, 0] as [number, number];
+
+  const radius = 5;
+
+  const upperBoundary = (x: number) =>
+    x <= 0 && x >= -radius ? -Math.sqrt(radius ** 2 - x ** 2) : NaN;
+
+  const lowerBoundary = (x: number) => (x <= 0 && x >= -radius ? 0 : NaN);
+
   return (
-    <Mafs width={500} height={500}>
+    <Mafs width={500} height={500} viewBox={{ x: [-6, 6], y: [-6, 6] }}>
       <Coordinates.Cartesian />
+      <Plot.Inequality
+        y={{
+          ">=": upperBoundary,
+          "<=": lowerBoundary,
+        }}
+        color={Theme.blue}
+      />
+      <Polygon points={[c, a, b]} color={Theme.blue} />
     </Mafs>
   );
 }
