740d1817ccf1af439f0ea0e7d9099f8e8f514ab9
nerfthisdev <353054@niuitmo.ru>
2024-12-17 20:25:04 +0300

    ✨ feat: complete graph changing logic

--- frontend/lab-app/src/components/RadioFieldSet.tsx
+++ frontend/lab-app/src/components/RadioFieldSet.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setRadius } from "../state/radius/radiusSlice";
+import { RootState } from "../state/store";
 
 interface Props {
   items: string[];
@@ -7,22 +10,33 @@ interface Props {
 }
 
 export function RadioFieldSet({ items, itemsvalues, children }: Props) {
+  const dispatch = useDispatch();
+
+  const selectedRadius = useSelector(
+    (state: RootState) => state.radiusReducer.value
+  );
+
+  const handleChange = (value: number) => {
+    dispatch(setRadius(value));
+  };
+
   return (
     <fieldset className="row mb-3">
       <legend className="col-form-label col-sm-2 pt-0">{children}</legend>
       <div className="col-sm-10">
-        {items.map((items, index) => (
-          <div className="form-check" key={items}>
+        {items.map((item, index) => (
+          <div className="form-check" key={item}>
             <input
               className="form-check-input"
               type="radio"
               name="gridRadios"
               id={`gridRadios${index}`}
               value={itemsvalues[index]}
-              defaultChecked={index === 0 ? true : false}
+              checked={selectedRadius === parseInt(itemsvalues[index])}
+              onChange={() => handleChange(parseInt(itemsvalues[index]))}
             />
             <label className="form-check-label" htmlFor={`gridRadios${index}`}>
-              {items}
+              {item}
             </label>
           </div>
         ))}
