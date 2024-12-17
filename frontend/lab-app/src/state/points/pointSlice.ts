e21fd5d2ce70fd41d11f0d74c21e968055821b02
nerfthisdev <353054@niuitmo.ru>
2024-12-17 21:01:16 +0300

    ✨ feat: change pointSlice and create reducers

--- frontend/lab-app/src/state/points/pointSlice.ts
+++ frontend/lab-app/src/state/points/pointSlice.ts
@@ -1,6 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { vec } from "mafs";
+import { PayloadAction } from "@reduxjs/toolkit";
 
-interface pointState {
-  xCoord: number;
-  yCoord: number;
+interface PointState {
+  pointsArray: vec.Vector2[];
 }
+
+const initialState: PointState = {
+  pointsArray: [],
+};
+
+const pointSlice = createSlice({
+  name: "points",
+  initialState,
+  reducers: {
+    addPoint: (state, action: PayloadAction<vec.Vector2>) => {
+      state.pointsArray.push(action.payload);
+    },
+    removePoint: (state, action: PayloadAction<number>) => {
+      state.pointsArray.splice(action.payload, 1);
+    },
+    clearPoints: (state) => {
+      state.pointsArray = [];
+    },
+  },
+});
+
+export const { addPoint } = pointSlice.actions;
+
+export default pointSlice.reducer;
