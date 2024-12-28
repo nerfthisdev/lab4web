6cf389f7e00ed2e2e12bfa7533c89a19582d2e9d
nerfthisdev <barm2001@gmail.com>
2024-12-28 12:28:36 +0300

    ✨ feat:  write function for button sent requests

--- frontend/lab-app/src/pages/AppPage.tsx
+++ frontend/lab-app/src/pages/AppPage.tsx
@@ -1,9 +1,10 @@
 import { FormSubmitButton } from "../components/FormSubmitButton";
 import { Graph } from "../components/Graph";
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 
 import { RadioFieldSet } from "../components/RadioFieldSet";
 import { TextInputWithLabel } from "../components/TextInputWithLabel";
+import { sendPoint } from "../services/apiService";
 
 const containerBoxStyle: CSSProperties = {
   height: "90vh",
@@ -13,6 +14,18 @@ const containerBoxStyle: CSSProperties = {
 
 export function AppPage() {
   const values = ["1", "2", "3", "4", "5"];
+  const [x, setX] = useState(Number);
+  const [y, setY] = useState(Number);
+
+  const handleClick = async () => {
+    var radius = 1;
+    try {
+      const response = await sendPoint({ x, y, radius });
+      console.log("Point saved to backend:", response);
+    } catch (error) {
+      console.error("Error saving point to backend");
+    }
+  };
 
   return (
     <div
@@ -20,14 +33,22 @@ export function AppPage() {
       style={containerBoxStyle}
     >
       <div className="box-with-inputs bg-dark text-white p-4 rounded shadow overflow-hidden ">
-        <TextInputWithLabel inputId="x2" label="X:" />
-        <TextInputWithLabel inputId="y2" label="Y:" />
+        <TextInputWithLabel
+          inputId="x2"
+          label="X:"
+          onInput={(e) => setX(Number((e.target as HTMLInputElement).value))}
+        />
+        <TextInputWithLabel
+          inputId="y2"
+          label="Y:"
+          onInput={(e) => setY(Number((e.target as HTMLInputElement).value))}
+        />
         <RadioFieldSet items={values} itemsvalues={values}>
           R:
         </RadioFieldSet>
 
         <div className="d-flex justify-content-center overflow-hidden">
-          <FormSubmitButton>Submit</FormSubmitButton>
+          <FormSubmitButton onClick={handleClick}>Submit</FormSubmitButton>
         </div>
       </div>
       <div className="ms-4 rounded-4 border border-1 overflow-hidden">
