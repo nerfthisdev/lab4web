da7206a2f40bc899b91c74f06530cfbdf9b3ab8b
nerfthisdev <barm2001@gmail.com>
2025-01-05 20:16:13 +0300

    ✨ feat: add login form

---
+++ frontend/lab-app/src/components/LoginForm.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function handleSubmit(event: React.SyntheticEvent) {
+  event?.preventDefault;
+}
+
+const USER_REGEX = /^[a-zA-z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%])/;
+
+export function LoginForm() {
+  const userRef = useRef<HTMLElement | null>(null);
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValiName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [validpassword, setValidPassword] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef?.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValiName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(password);
+    console.log(result);
+    console.log(password);
+    setValidPassword(result);
+    const match = password === matchPassword;
+    setValidMatch(match);
+  }, [password, matchPassword]);
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4" style={{ width: "20rem" }}>
+        <form onSubmit={handleSubmit} className="needs-validation">
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="login"
+              placeholder="Login"
+              required
+            />
+            <label htmlFor="login">Login</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="password"
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
