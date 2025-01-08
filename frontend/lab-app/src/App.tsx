616fb62d821ddc9d1560fccc8c41be2ad15ea82f
nerfthisdev <barm2001@gmail.com>
2025-01-08 12:48:14 +0300

    ✨ feat: add private routes

--- frontend/lab-app/src/App.tsx
+++ frontend/lab-app/src/App.tsx
@@ -1,21 +1,55 @@
 import { NavBar } from "./components/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { AppPage } from "./pages/AppPage";
 import { SignUpPage } from "./pages/SignUpPage";
-import { Login } from "./pages/LoginPage";
+import { LoginPage } from "./pages/LoginPage";
+import { isAuthenticated } from "./services/apiService";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/app" element={<AppPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={<Navigate to={isAuthenticated() ? "/app" : "/login"} />}
+        />
+
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PrivateRoute>
+              <About />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/app"
+          element={
+            <PrivateRoute>
+              <AppPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
