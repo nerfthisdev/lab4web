a203fc0d88a9bf1a36a6b3577366f56277c8b411
nerfthisdev <barm2001@gmail.com>
2025-01-08 12:48:29 +0300

    ✨ feat: add cors filter

--- backend/src/main/java/filters/CORSFilter.java
+++ backend/src/main/java/filters/CORSFilter.java
@@ -1,4 +1,32 @@
 package filters;
 
-public class CORSFilter {
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final String ALLOWED_ORIGINS = "http://127.0.0.1:5173";
+    private static final String ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
+    private static final String ALLOWED_HEADERS = "origin, content-type, accept, authorization";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+
+        if ("OPTIONS".equals(requestContext.getMethod())) {
+            requestContext.abortWith(Response.status(Response.Status.NO_CONTENT).build());
+        }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", ALLOWED_ORIGINS);
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    }
 }
