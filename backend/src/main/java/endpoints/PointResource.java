8a3713fae76dc8e32efd71fe87eef6f90c592285
nerfthisdev <barm2001@gmail.com>
2025-01-08 11:41:44 +0300

    ✨ feat: add functional point resource

--- backend/src/main/java/endpoints/PointResource.java
+++ backend/src/main/java/endpoints/PointResource.java
@@ -1,18 +1,24 @@
 package endpoints;
 
+import elasticlogic.ElasticClient;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.io.IOException;
+
 @Path("/point")
 public class PointResource {
+    @Inject
+    ElasticClient elasticClient;
 
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String sayHello() {
-        return "{\"hello\":\"world\"}\"";
+    public String getPoints() throws IOException {
+        return elasticClient.getPoints();
     }
 
 }
